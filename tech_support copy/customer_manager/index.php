d6d85713b71708a7da1b3aa3f97f82f9f2b50513
Rus <rus.zakirov@triosstudent.com>
2024-10-20 19:35:01 -0400

    Added table to customer_manager's index.php and also updated path in index.php to customer_manager index page

---
+++ tech_support copy/customer_manager/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Manage Customers</title>
+   <link rel="stylesheet" href="/PHP_Assignment_5/tech_support copy/main.css">
+</head>
+<body>
+   <?php include '../view/header.php'; ?>
+   <main>
+      <table>
+         <th>Name</th>
+         <th>Email Address</th>
+         <th>City</th>
+      </table>
+   </main>
+
+</body>
+<?php include '../view/footer.php'; ?> 
+</html>
